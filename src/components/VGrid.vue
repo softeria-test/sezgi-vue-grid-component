0b202e65a420ef3d4019a2ec62a9d753caab7774
Sezgi <szqii.altn@gmail.com>
2023-11-21 13:56:18 +0300

    create: components

---
+++ src/components/VGrid.vue
@@ -0,0 +1,82 @@
+<template>
+    <div class="grid-row">
+      <table class="table" v-if="table && table.data">
+        <caption class="title">
+          Example Table
+        </caption>
+        <tbody >
+        <tr
+            v-for="(row, rowIndex) in table.data.rows"
+            :key="rowIndex"
+            class="table-row"
+
+        >
+          <th
+              v-for="(cellValue, columnIndex) in row.cells"
+              :rowspan="row.headerCellDetails && row.headerCellDetails[columnIndex]['rowspan']"
+              :colspan="row.headerCellDetails && row.headerCellDetails[columnIndex]['colspan']"
+              :key="columnIndex"
+              :style="{
+              textAlign: setAlignment(
+                table,
+                row,
+                Number(columnIndex),
+                'horizontal'
+              ),
+              verticalAlign: setAlignment(
+                table,
+                row,
+                Number(columnIndex),
+                'vertical'
+              ),
+            }"
+          >
+            <single-cell
+                :cell-value="cellValue ? cellValue.toString() : ''"
+                :is-header="row.rowType ? checkIsHeader(row.rowType.toString()) : false"
+                :is-header-leaf="checkIsHeaderLeaf(row, columnIndex)"
+            />
+          </th>
+        </tr>
+        </tbody>
+      </table>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import {ITable} from "@/types";
+import SingleCell from "@/components/partials/SingleCell.vue";
+import {checkIsHeaderLeaf} from "@/helpers/checkIsHeaderLeaf";
+import {checkIsHeader} from "@/helpers/checkIsHeader";
+import {setAlignment} from "@/helpers/setAlignment";
+export default Vue.extend({
+  name: 'VGrid',
+  methods: {setAlignment, checkIsHeader, checkIsHeaderLeaf},
+  components: {SingleCell},
+  props: {
+    table: Object as () => ITable | null,
+  },
+});
+
+</script>
+
+
+<style scoped lang="scss">
+table {
+  border-collapse: collapse;
+}
+
+tr,
+th {
+  border: 1px solid black;
+}
+
+.grid-row {
+  margin: auto auto;
+
+  .title {
+    margin-bottom: 2rem;
+  }
+}
+</style>
---
+++ src/components/partials/SingleCell.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="cell" :class="isHeaderLeaf && isHeader ? 'clickable' : ''">
+    <span class="cell-value">{{ text }}</span>
+    <div v-if="isHeaderLeaf && isHeader">
+      <font-awesome-icon icon="fa fa-chevron-down" class="arrow-icon" ></font-awesome-icon>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue, {ref, watch} from "vue";
+import {setCellValue} from "@/helpers/setCellValue";
+
+export default Vue.extend({
+  name: "SingleCell",
+  props : {
+    cellValue: String,
+    isHeaderLeaf: Boolean,
+    isHeader: Boolean,
+  },
+  setup(props) {
+    const text = ref("");
+    text.value = setCellValue(props.cellValue, props.isHeader);
+
+    watch(() => props.cellValue, () => {
+      text.value = setCellValue(props.cellValue, props.isHeader);
+    });
+
+    return {
+      text,
+    };
+  },
+});
+
+// const props = defineProps({
+//   cellValue: {
+//     type: String,
+//     required: true,
+//   },
+//   isHeaderLeaf: {
+//     type: Boolean,
+//     required: true,
+//   },
+//   isHeader: {
+//     type: Boolean,
+//     required: true,
+//   },
+// });
+//
+// const text = ref("");
+// text.value = setCellValue(props.cellValue, props.isHeader);
+
+</script>
+
+<style scoped lang="scss">
+.cell {
+  display: inline-flex;
+  padding: 0 1rem;
+
+  &.clickable {
+    cursor: pointer;
+    transition: background-color 0.2s ease-in-out;
+
+    &:hover {
+      background-color: #eeeeee;
+    }
+  }
+
+  .arrow-icon {
+    font-size: 10px;
+    align-self: center;
+    margin-left: 4px;
+  }
+}
+</style>
